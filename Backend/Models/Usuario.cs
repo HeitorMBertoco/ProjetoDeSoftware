c3381146be06fa8daacde67edd4c615db3935cc3
João <jgablopes2008@gmail.com>
2026-03-13 16:53:54 -0300

    Início do backend

---
+++ Backend/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models;
+
+public class Usuario
+{
+    [Key] public Guid Id { get; set; }
+    [StringLength(100)] public required String Nome { get; set; }
+    [StringLength(150)] public String? Sobrenome { get; set; }
+    [StringLength(100)] public required String Login { get; set; }
+    [StringLength(32)] public required String Senha { get; set; }
+}
