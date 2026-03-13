41becc1828aec05ddb80272d02dd1440be27491b
Heitor <hmbertoco@gmail.com>
2026-03-13 16:51:03 -0300

    Stoped

--- Frontend/projetofaltas/app/page.tsx
+++ Frontend/projetofaltas/app/page.tsx
@@ -1,9 +1,14 @@
-
+import { Input } from "@/Components/ui/input";
+import "./globals.css"
 export default function Home() {
   return (
       <>
-
-
+        <div className="root">
+          <div className="main antialised">
+            <h1>Realize login</h1>
+              <Input></Input>
+          </div>
+        </div>   
       </>
   );
 }
