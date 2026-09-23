4fbdedc383b6adc3e179331bfb3268fb08e2ba9b
João <jgablopes2008@gmail.com>
2026-09-23 11:07:33 -0300

    Adição do campo EntradaSaida no registro

--- Backend/Migrations/20260923134501_RegistroEntradaSaida.Designer.cs
+++ Backend/Migrations/20260923134501_RegistroEntradaSaida.Designer.cs
@@ -4,6 +4,7 @@ using Backend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend.Migrations
 {
     [DbContext(typeof(BackendContext))]
-    partial class BackendContextModelSnapshot : ModelSnapshot
+    [Migration("20260923134501_RegistroEntradaSaida")]
+    partial class RegistroEntradaSaida
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -73,7 +76,7 @@ namespace Backend.Migrations
                     b.Property<DateTime>("Data")
                         .HasColumnType("datetime2");
 
-                    b.Property<string>("EntradaSaida")
+                    b.Property<string>("EntradaSaisa")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
